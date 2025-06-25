b8268d68532a1553995865c03f225adb2e693d7e
romahawk <romazuryk@gmail.com>
2025-06-26 00:00:18 +0200

    update 1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -5,7 +5,7 @@ export const DEFAULT_LIMIT = 15;
 
 axios.defaults.baseURL = 'https://pixabay.com/api';
 axios.defaults.params = {
-  key: '50351330-2fc0fc2b1e2442bcaa307ce6b',
+  key: '51053384-5356fc0331195a75022768729',
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: 'true',
